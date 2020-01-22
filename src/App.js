b0e0271636516da36db9a3ccdae4c758232ee4cf
gorilla-Kim <00ghks22@naver.com>
2020-01-23 01:38:11 +0900

    create customer componenet layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'id': 1,
+  'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
+  'name' : 'gorilla-kim',
+  'birthday': '960322',
+  'gender': 'Man',
+  'job': 'Student'
+} 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Customer 
+      id={customer.id}
+      image={customer.image}
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
---
+++ src/components/Customer.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Customer(props) {
+    return (
+        <div>
+            <CustomerProfile 
+                id={props.id}
+                name={props.name}
+                image={props.image}
+            />
+            <CustomerInfo 
+                birthday={props.birthday}
+                gender={props.gender}
+                job={props.job}
+            />
+        </div>
+    );
+}
+
+function CustomerProfile (props) {
+    return (
+        <div>
+            <img src={props.image} alt="profile"/>
+            <h2>{props.name}({props.id})</h2>
+        </div>
+    )
+}
+
+function CustomerInfo (props) {
+    return (
+        <div>
+            <p>{props.birthday}</p>
+            <p>{props.gender}</p>
+            <p>{props.job}</p>
+        </div>
+    )
+}
