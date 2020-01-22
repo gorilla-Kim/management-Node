44cc2506d554acc95cccc216f0aef6d0522ba364
gorilla-Kim <00ghks22@naver.com>
2020-01-23 02:22:35 +0900

    Apply Materials UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -34,36 +47,41 @@ const customers = [
   }   
 ];
 
-function App() {
+function App(props) {
+
+  const { classes } = props;
+  console.log(classes);
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>번호</TableCell>
-          <TableCell>이미지</TableCell>
-          <TableCell>이름</TableCell>
-          <TableCell>생년월일</TableCell>
-          <TableCell>성별</TableCell>
-          <TableCell>직업</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {customers.map(customer => {
-          return (
-            <Customer 
-              key={customer.id}
-              id={customer.id}
-              image={customer.image}
-              name={customer.name}
-              birthday={customer.birthday}
-              gender={customer.gender}
-              job={customer.job}
-            />
-          )  
-        })}
-      </TableBody>
-    </Table>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(customer => {
+            return (
+              <Customer 
+                key={customer.id}
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+              />
+            )  
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
