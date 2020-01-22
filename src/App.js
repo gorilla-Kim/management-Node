c5634daa8baf422b683455b46a661d7f0b967cb5
gorilla-Kim <00ghks22@naver.com>
2020-01-23 01:44:44 +0900

    create dummy data :: customers

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'id': 1,
-  'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
-  'name' : 'gorilla-kim',
-  'birthday': '960322',
-  'gender': 'Man',
-  'job': 'Student'
-} 
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
+    'name' : 'gorilla-kim',
+    'birthday': '960322',
+    'gender': 'Man',
+    'job': 'Student'
+  },
+  {
+    'id': 2,
+    'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
+    'name' : 'gorilla-kim',
+    'birthday': '960322',
+    'gender': 'Man',
+    'job': 'Student'
+  },
+  {
+    'id': 3,
+    'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
+    'name' : 'gorilla-kim',
+    'birthday': '960322',
+    'gender': 'Man',
+    'job': 'Student'
+  }   
+];
 
 function App() {
   return (
-    <Customer 
-      id={customer.id}
-      image={customer.image}
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {
+        customers.map(customer => 
+          <Customer 
+            key={customer.id}
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        )
+      }
+    </div>
   );
 }
 
