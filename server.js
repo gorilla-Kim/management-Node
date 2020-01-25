20f9ba4a4a958234e679de9ceb779e4fadb9b6d5
gorilla-Kim <00ghks22@naver.com>
2020-01-25 19:38:35 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,34 +9,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// database
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection =  mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 // router
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
-          'name' : 'gorilla-kim',
-          'birthday': '960322',
-          'gender': 'Man',
-          'job': 'Student'
-        },
-        {
-          'id': 2,
-          'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
-          'name' : 'gorilla-kim',
-          'birthday': '960322',
-          'gender': 'Man',
-          'job': 'Student'
-        },
-        {
-          'id': 3,
-          'image': 'http://www.gorillawebmarketing.co.uk/wp-content/uploads/2014/02/a03-10-gorilla-computer-laptop-1-300x226.jpg',
-          'name' : 'gorilla-kim',
-          'birthday': '960322',
-          'gender': 'Man',
-          'job': 'Student'
-        }   
-    ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 /* ====================== function code ====================== */
