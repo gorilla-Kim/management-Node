3da391ec1bf90df1bd1b03d840afd9d16ae68788
gorilla-Kim <00ghks22@naver.com>
2020-01-29 00:07:46 +0900

    connected express and react

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/static', express.static('./client/build/static'));
 app.use('/image', express.static('./uploads'));
 
 // database
@@ -49,6 +50,10 @@ const upload = multer({
 });
 
 // router
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
+
 app.get('/api/customers', (req, res) => {
   connection.query(
     "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
